3d78c09e506460ffdfe9b840b137de302d502ebb
Venkata Lakshman Nimmagadda <lakshman@Venkatas-MacBook-Air.local>
2023-12-11 20:53:53 -0500

    Commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+
+
+
+function Footer() {
+    return (
+        <footer className="bottom">
+    
+        <div className="center">
+            All rights reserved &copy; Venkata Lakshman
+        </div>
+    
+</footer>
+  
+    );
+  }
+  
+  export default Footer;
