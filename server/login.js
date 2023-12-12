dec95868df35b11eccd45e0cc2363f10da589378
Venkata Lakshman Nimmagadda <lakshman@Venkatas-Air.lan>
2023-12-12 12:58:46 -0500

    login mongodb

--- server/login.js
+++ server/login.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 const loginModel=require('./models/loginModel')
-let url='mongodb://127.0.0.1:27017/mongodb_demo'
+let url='mongodb+srv://vnimmag3:Lakshman1999@cluster0.daon64g.mongodb.net/personal-budget'
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
         .then(()=>{
             console.log("Connected to Db")
